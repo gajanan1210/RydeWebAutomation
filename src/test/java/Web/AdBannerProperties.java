fd371389e875cc033880f1ea274b16edb9118724
gajanan1210 <45044405+gajanan1210@users.noreply.github.com>
2024-05-10 11:16:24 +0530

    Update AdBannerProperties.java

--- src/test/java/Web/AdBannerProperties.java
+++ src/test/java/Web/AdBannerProperties.java
@@ -54,32 +54,14 @@ public class AdBannerProperties {
 			System.out.println("Ad banner does not appear fixed at the bottom of the screen while scrolling the page.");
 		}
 
-		/*
-		 * // Compare the initial and current positions if (initialY == currentY) {
-		 * System.out.
-		 * println("Ad banner remains fixed at the bottom of the screen while scrolling the page."
-		 * ); } else { System.out.
-		 * println("Ad banner does not remain fixed at the bottom of the screen while scrolling the page."
-		 * ); }
-		 */
+		
 
 		// Verify close button
 
 		WebElement closeButton = driver.findElement(By.xpath("//i[@class='eicon-close']"));
 		closeButton.click();
 
-		/*
-		 * 
-		 * if (element.isDisplayed()) {
-		 * 
-		 * System.out.
-		 * println("Ad banner remains visible after clicking the 'Close' button.");
-		 * 
-		 * } else {
-		 * 
-		 * System.out.println("Ad banner disappears when the 'Close' button is clicked."
-		 * ); }
-		 */
+		
 		// Thread.sleep(2000);
 
 		try {
@@ -90,37 +72,12 @@ public class AdBannerProperties {
 			System.out.println("Ad banner disappears when the 'Close' button is clicked.");
 
 		} catch (TimeoutException e) {
-			// Handle timeout exception if element does not become invisible within the
-			// specified timeout
-			// For example, log an error message, take a screenshot, or retry the action
+		
 			System.out.println("Ad banner did not disappear when the 'Close' button is clicked.");
 			// TakeScreenshot.capture(driver, "element_not_invisible");
 		}
 
-		/*
-		 * 
-		 * 
-		 * 
-		 * WebElement elementAfterClose = driver.findElement(By.xpath(
-		 * "//div[@class='elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-1535567b']//div[@class='elementor-widget-wrap elementor-element-populated e-swiper-container']"
-		 * ));
-		 * 
-		 * // Verify if the ad banner disappears after clicking the close button if
-		 * (!elementAfterClose.isDisplayed()) {
-		 * System.out.println("Ad banner disappears when the 'Close' button is clicked."
-		 * ); } else { System.out.
-		 * println("Ad banner remains visible after clicking the 'Close' button."); }
-		 * 
-		 */
-
-		/*
-		 * // Verify if the ad banner is no longer visible if (element.isDisplayed()) {
-		 * System.out.
-		 * println("Ad banner remains visible after clicking the 'Close' button."); }
-		 * else {
-		 * System.out.println("Ad banner disappears when the 'Close' button is clicked."
-		 * ); }
-		 */
+		
 
 	}
 
